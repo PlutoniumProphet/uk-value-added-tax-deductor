5f121c9f6c3691961e32a331f52e2933e01c6b52
grahamkesley <grahamkesley@gmail.com>
2020-06-30 19:11:52 +0100

    working script with verbose printing option and adjustable VAT rate

--- vatd.py
+++ vatd.py
@@ -3,36 +3,43 @@
 
 import argparse
 
-parser = argparse.ArgumentParser(prog='VAT Deductor',
+
+parser = argparse.ArgumentParser(
+    prog='VAT Deductor',
     description='Displays the VAT deductable from a total receipt value')
-parser.add_argument('--version', action='version', version='%(prog)s 1.0')
-parser.add_argument('--rate', type=float, action='store',
-                    nargs='?', default='1.2',
-                help='enter VAT rate as a decimal. Default is 1.2')
-parser.add_argument('-v', '--verbose', action='store_true',
-                help='displays Total Amount, VAT deductable & pretax values')
-parser.add_argument('Total', metavar='T', type=float, action='store',
-                help='amount of TOTAL receipt value for processing')
+parser.add_argument(
+    '--version',
+    action='version',
+    version='%(prog)s 1.0')
+parser.add_argument(
+    '--rate',
+    type=float,
+    action='store',
+    nargs='?',
+    default='1.2',
+    help='enter VAT rate as a decimal. Default is 1.2')
+parser.add_argument(
+    '-v',
+    '--verbose',
+    action='store_true',
+    help='displays Total Amount, VAT deductable & pretax values')
+parser.add_argument(
+    'total',
+    metavar='T',
+    type=float,
+    action='store',
+    help='amount of TOTAL receipt value for processing')
 
+args = parser.parse_args()
 
-parser.parse_args()
+net_value = round(args.total / args.rate, 2)
+refund_vat = round(args.total - net_value, 2)
 
-#VAT_RATE = 1.20
-
-
-#def calc():
- #   try:
-  #      receipt_value = input("Enter receipt value: ")
-   #     receipt_value = float(receipt_value)
-    #    net_value = float(receipt_value) / VAT_RATE
-     #   refund_vat = round(float(receipt_value) - net_value, 2)
-      #  print(refund_vat)
-       # if input("Run again? ") == "y":
-        #    calc()
-       # else:
-        #    print("Bye!")
-    #except ValueError:
-     #   print("You can only enter numbers!")
-      #  calc()
-
-#calc()
+if args.verbose:
+    print(f"""
+Total Receipt:  £{args.total}
+Net Value:      £{net_value}
+Deductable VAT: £{refund_vat}
+          """)
+else:
+    print(f"Deductable VAT: £{refund_vat}")
